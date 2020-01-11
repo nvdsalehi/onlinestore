e267c39661b4b91794415395dabddf5dbd4e7e28
Navid Salehi <nvdsalehi@gmail.com>
2020-01-11 19:34:30 +0330

    Add profiler package

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -4,16 +4,17 @@
 namespace App\Controller;
 
 
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-class DefaultController
+class DefaultController extends AbstractController
 {
     /**
      * @Route("/")
      */
     public function index()
     {
-        return new Response('This is test');
+        return $this->render('base.html.twig');
     }
 }
