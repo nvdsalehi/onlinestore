0738b2b4c93b3c21b2178cea6e0872f680e9bb6b
Navid Salehi <nvdsalehi@gmail.com>
2020-01-11 19:07:26 +0330

    Add annotations package

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,19 @@
+<?php
+
+
+namespace App\Controller;
+
+
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class DefaultController
+{
+    /**
+     * @Route("/")
+     */
+    public function index()
+    {
+        return new Response('This is test');
+    }
+}
